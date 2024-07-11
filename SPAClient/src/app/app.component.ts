e5fc05c03b89bbb2856b71220bb4481e2be2118f
LinhDancute <dannguyen11003@gmail.com>
2024-07-11 23:24:12 +0700

    [SPAClient] create landingpage/pagecontent

--- SPAClient/src/app/app.component.ts
+++ SPAClient/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FooterComponent} from "./footer/footer.component";
+import {LandingpageComponent} from "./landingpage/landingpage.component";
+import {PageContentComponent} from "./page-content/page-content.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent, LandingpageComponent, PageContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
