ae7b31a36db0f924ab35701a55b920133de03632
LinhDancute <dannguyen11003@gmail.com>
2024-07-12 21:42:59 +0700

    [SPAClient] - Fix routes and html template

--- SPAClient/src/app/login/login.component.ts
+++ SPAClient/src/app/login/login.component.ts
@@ -1,37 +1,38 @@
 import {Component, inject} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {FormsModule} from "@angular/forms";
-import {Router} from "@angular/router";
+import {Router, RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    RouterLink
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  loginObj:any = {
-    "Email": "",
-    "Password": ""
-  };
-
-  http = inject(HttpClient)
-  constructor(private router: Router) {
-
-  }
-  onLogin(){
-    debugger;
-    this.http.post("http://localhost:7002", this.loginObj).subscribe((response:any)=>{
-      if (response.result){
-        this.loginObj = response;
-        alert("Đăng nhập thành công");
-        this.router.navigateByUrl("pageContent");
-      } else {
-        alert("Sai email hoặc mật khẩu");
-      }
-    })
-  }
+  // loginObj:any = {
+  //   "Email": "",
+  //   "Password": ""
+  // };
+  //
+  // http = inject(HttpClient)
+  // constructor(private router: Router) {
+  //
+  // }
+  // onLogin(){
+  //   debugger;
+  //   this.http.post("http://localhost:7002/Login", this.loginObj).subscribe((response:any)=>{
+  //     if (response.result){
+  //       this.loginObj = response;
+  //       alert("Đăng nhập thành công");
+  //       this.router.navigateByUrl("pageContent");
+  //     } else {
+  //       alert("Sai email hoặc mật khẩu");
+  //     }
+  //   })
+  // }
 }
--- SPAClient/src/app/register/register.component.ts
+++ SPAClient/src/app/register/register.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {FormsModule} from "@angular/forms";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-register',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    RouterLink
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
