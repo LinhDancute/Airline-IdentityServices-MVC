9655bfee21b33417e8ae69f4f47fee6994b667fd
LinhDancute <dannguyen11003@gmail.com>
2024-07-13 23:50:56 +0700

    [SPAClient] - Create flight-search layout

--- SPAClient/src/app/app.routes.ts
+++ SPAClient/src/app/app.routes.ts
@@ -4,12 +4,17 @@ import {LoginComponent} from "./login/login.component";
 import {RegisterComponent} from "./register/register.component";
 import {MainComponent} from "./main/main.component";
 import {FlightListComponent} from "./flight-list/flight-list.component";
+import {FlightSearchComponent} from "./flight-search/flight-search.component";
 
 export const routes: Routes = [
   { path: '', redirectTo: '/main', pathMatch: 'full' },
   {
     path: 'main',
     component: MainComponent
+  },
+  {
+    path: 'flight-search',
+    component: FlightSearchComponent
   },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
